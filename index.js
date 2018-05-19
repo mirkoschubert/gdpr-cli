7263cdb3ecfd5ce6837950910016ab2a270b2385
Mirko Schubert <mirkoschubert@gmail.com>
2018-05-19 19:31:55 +0200

    new concept: own task manager

--- index.js
+++ index.js
@@ -6,26 +6,36 @@ const app = require('commander');
 const Tasks = require('./lib/tasks');
 const UI = require('./lib/ui');
 
-const tasks = new Tasks(); // initialize the task runner
+
 const ui = new UI(); // initialize the UI
 
 app
-  .version(require('./package.json').version, '-v, --version');
+  .version(require('./package.json').version, '-V, --version')
+  .option('-v, --verbose', 'shows you every single step')
+  .option('-s, --silent', 'shows only the results of the analysis');
 
 app
   .command('scan [url]')
   .alias('s')
   .description('Scans an url')
-  .option('-f, --fonts', 'checks if any font is loading externally')
+  .option('-h, --html', 'reads only the HTML files', false)
+  .option('-f, --fonts', 'checks if any font is loading externally', true)
   .option('-p, --prefetching', 'checks for DNS prefetching')
-  .option('-r, --recursive', 'tries to follow links to check every internal site')
-  .option('-V, --verbose', 'shows you every single step')
-  .option('-S, --silent', 'shuts up')
-  .action((url, options) => {
-    console.log(options);
-    if (options.verbose) ui.set('verbose');
-    if (options.silent) ui.set('silent');
+  .option('-r, --recursive', 'tries to follow links to check every internal site', false)
+  .action((url, args) => {
+    if (args.parent.verbose && args.parent.silent) {
+      ui.error('\nYou have to choose between silent or verbose mode!\n');
+    } else {
+      if (args.parent.verbose) ui.set('verbose');
+      if (args.parent.silent) ui.set('silent');
+      //console.log(args);
+      const tasks = new Tasks(url, ui); // initialize the task runner
 
+      if (args.fonts) tasks.new('fonts');
+      if (args.prefetching) tasks.new('prefetching');
+
+      tasks.run();
+    }
   });
 
 app
