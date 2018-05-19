c1041d59ad054769baa5ce5b17bc24a01eb90e0c
Mirko Schubert <mirkoschubert@gmail.com>
2018-05-19 10:34:35 +0200

    new custom ui & tasks classes

--- index.js
+++ index.js
@@ -3,20 +3,31 @@
 'use strict'
 
 const app = require('commander');
-const tasks = require('./lib/tasks');
-const Parser = require('./lib/parser');
+const Tasks = require('./lib/tasks');
+const UI = require('./lib/ui');
 
-
+const tasks = new Tasks(); // initialize the task runner
+const ui = new UI(); // initialize the UI
 
 app
-  .version(require('./package.json').version);
+  .version(require('./package.json').version, '-v, --version');
 
 app
   .command('scan [url]')
+  .alias('s')
   .description('Scans an url')
-  .action(url => {
-    tasks.run(url);
+  .option('-f, --fonts', 'checks if any font is loading externally')
+  .option('-p, --prefetching', 'checks for DNS prefetching')
+  .option('-r, --recursive', 'tries to follow links to check every internal site')
+  .option('-V, --verbose', 'shows you every single step')
+  .option('-S, --silent', 'shuts up')
+  .action((url, options) => {
+    console.log(options);
+    if (options.verbose) ui.set('verbose');
+    if (options.silent) ui.set('silent');
+
   });
 
 app.parse(process.argv);
-if (app.args.length === 0) app.help();
+console.log(app.args.length);
+if (!app.args.length) app.help();
